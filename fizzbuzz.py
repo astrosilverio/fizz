bae3ba14d16c862b40225da581c2798f3c82dbb8
Katie Silverio <silverio@dusty.local>
2013-09-30 18:18:41 -0400

    added fizz and buzz

--- fizzbuzz.py
+++ fizzbuzz.py
@@ -2,5 +2,9 @@ for i in range(1, 100):
 
     if i % 15 == 0:
         print "fizzbuzz"
+    elif i % 5 == 0:
+        print "buzz"
+    elif i % 3 == 0:
+        print "fizz"
     else:
         print i
